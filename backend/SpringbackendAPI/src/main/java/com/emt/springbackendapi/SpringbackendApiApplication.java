ba86fdbeb0c1ffce43dff1072fe39ca5c1745ca1
MarkoMinovski <minovskimarco@gmail.com>
2025-03-08 02:59:33 +0100

    Setup spring

---
+++ backend/SpringbackendAPI/src/main/java/com/emt/springbackendapi/SpringbackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.emt.springbackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbackendApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbackendApiApplication.class, args);
+    }
+
+}
---
+++ backend/SpringbackendAPI/src/test/java/com/emt/springbackendapi/SpringbackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emt.springbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbackendApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
